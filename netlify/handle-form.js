d304540c96134b0ec1782a904d32ba41ea88f55d
Zach Urich <re.jo@live.com>
2020-01-28 20:07:12 -0600

    Adding email function

--- netlify/handle-form.js
+++ netlify/handle-form.js
@@ -1,1 +1,22 @@
-console.log("Yo");
+require("dotenv").config();
+const fetch = require("node-fetch");
+const { BUTTONDOWN_API_KEY } = process.env;
+exports.handler = async event => {
+  const { email } = JSON.parse(event.body).payload;
+  console.log(`Recieved an email address: ${email}`);
+  try {
+    const response = await fetch("https://api.buttondown.email/v1/subscribers", {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email })
+    });
+
+    console.log("Response: ", response.json());
+    return response.json();
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify(error) };
+  }
+};
